782065313d1cfd5c55799d4b72b5651fb91a4a3e
John Fajardo <fajardocj@gmail.com>
2019-07-08 21:30:23 -0400

    Starts work on tty-prompt

--- main.rb
+++ main.rb
@@ -5,8 +5,18 @@ require 'pry'
 require 'require_all'
 require 'rake'
 require 'tty-prompt'
-require_all 'classes'
-
+require 'active_record'
+require_relative './config/config'
+require_relative './classes/technician'
 
 prompt = TTY::Prompt.new
 prompt.ask('What is your name?', default: ENV['USER'])
+
+techs = Technician.pluck(:name)
+orders = JobOrder.pluck(:job_name)
+clients = Client.pluck(:client_name)
+
+options = ["View technicians", "View orders", "View clients"]
+
+
+prompt.select("What do you want to do?", options, per_page: 15 )
